b825cfbe522ea97cf9c62dc2d3e45847a9283bdc
Dmitry Sviridenko <letehaha@gmail.com>
2020-10-31 01:32:08 +0200

    Feat: update Transaction model

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -2,38 +2,43 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const Transaction = new Schema({
-  account: {
-    type: Schema.Types.ObjectId,
-    ref: 'Account',
-    required: [true, 'Transaction account is required'],
+const Transaction = new Schema(
+  {
+    account: {
+      type: Schema.Types.ObjectId,
+      ref: 'Account',
+      required: [true, 'Transaction account is required'],
+    },
+    amount: {
+      type: Number,
+      required: [true, 'Transaction amount is required'],
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: [true, 'Transaction category is required'],
+    },
+    note: {
+      type: String,
+    },
+    paymentType: {
+      type: Schema.Types.ObjectId,
+      ref: 'PaymentType',
+      required: [true, 'Transaction paymentType is required'],
+    },
+    time: {
+      type: Date,
+      required: [true, 'Transaction time is required'],
+    },
+    type: {
+      type: Schema.Types.ObjectId,
+      ref: 'TransactionType',
+      required: [true, 'Transaction type is required'],
+    },
   },
-  amount: {
-    type: Number,
-    required: [true, 'Transaction amount is required'],
+  {
+    timestamps: true,
   },
-  category: {
-    type: Schema.Types.ObjectId,
-    ref: 'Category',
-    required: [true, 'Transaction category is required'],
-  },
-  note: {
-    type: String,
-  },
-  paymentType: {
-    type: Schema.Types.ObjectId,
-    ref: 'PaymentType',
-    required: [true, 'Transaction paymentType is required'],
-  },
-  time: {
-    type: Date,
-    required: [true, 'Transaction time is required'],
-  },
-  type: {
-    type: Schema.Types.ObjectId,
-    ref: 'TransactionType',
-    required: [true, 'Transaction type is required'],
-  },
-});
+);
 
 module.exports = mongoose.model('Transaction', Transaction);
