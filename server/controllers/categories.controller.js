f3199626fbcd0aac3eef44078c840d337a5f1d8a
Dmitry Sviridenko <letehaha@gmail.com>
2020-11-22 16:16:32 +0200

    Feat: improve categories data response

--- server/controllers/categories.controller.js
+++ server/controllers/categories.controller.js
@@ -2,11 +2,39 @@ const { Categories } = require('@models');
 
 exports.getCategories = async (req, res, next) => {
   const { id } = req.user;
+  const { rawCategories } = req.query;
 
   try {
-    const data = await Categories.getCategories({ id });
+    let data = await Categories.getCategories({ id });
 
-    return res.status(200).json({ response: data });
+    if (rawCategories !== undefined) {
+      return res.status(200).json({ response: data });
+    }
+
+    data = data.map((item) => item.dataValues);
+
+    const objectGraph = (items) => {
+      const itemsById = {};
+      const roots = [];
+
+      // build an id->object mapping, so we don't have to go hunting for parents
+      items.forEach((item) => {
+        itemsById[item.id] = item;
+        // eslint-disable-next-line no-param-reassign
+        item.subCategories = [];
+      });
+
+      items.forEach((item) => {
+        const { parentId } = item;
+        // if parentId is null, this is a root; otherwise, it's parentId's kid
+        const nodes = !parentId ? roots : itemsById[parentId].subCategories;
+        nodes.push(item);
+      });
+
+      return roots;
+    };
+
+    return res.status(200).json({ response: objectGraph(data) });
   } catch (err) {
     return next(new Error(err));
   }
