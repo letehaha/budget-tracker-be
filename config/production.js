f8d42116516d38187ba180a053877051a6519777
Dmitry Sviridenko <letehaha@gmail.com>
2020-12-16 09:47:22 +0200

    Feat: update .env

--- config/production.js
+++ config/production.js
@@ -6,7 +6,7 @@ module.exports = {
   port: process.env.SERVICES_API_PORT,
   apiPrefix: process.env.SERVICES_API_PREFIX,
   jwtSecret: process.env.SERVICES_JWT_SECRET,
-  hostWebhooksCallback: process.env.SERVICES_WEBHOOK_CALLBACK,
+  hostWebhooksCallback: process.env.SERVICES_WEBHOOK_CALLBACK_HOST,
   db: {
     host: process.env.SERVICES_API_DB_HOST,
     user: process.env.SERVICES_API_DB_USER,
