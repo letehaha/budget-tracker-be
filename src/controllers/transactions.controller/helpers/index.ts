d67f05f05efbbbe30e0edb705c2e9fb254d58e82
Dmitry Sviridenko <letehaha@gmail.com>
2022-09-17 15:50:10 +0300

    chore: Remove useless code

--- src/controllers/transactions.controller/helpers/index.ts
+++ src/controllers/transactions.controller/helpers/index.ts
@@ -5,12 +5,3 @@ export const validateTransactionAmount = (amount): void => {
   if (amount < 0) throw new ValidationError({ message: 'Amount should be positive.' });
   if (!Number.isInteger(amount)) throw new ValidationError({ message: 'Amount should be an integer.' });
 };
-
-export const validateTransactionOppositeChange = (
-  id: number,
-  oppositeId: number,
-) => {
-  if (id === oppositeId) {
-    throw new ValidationError({ message: 'You cannot edit or delete opposite transaction.'})
-  }
-};
