9d53d70d01a04cc6e1498d19c1a066a86409b8f6
Dmitry Sviridenko <letehaha@gmail.com>
2022-09-29 23:39:28 +0300

    feat: Add user exchange rates model

---
+++ src/migrations/1664482567667-user-exchange-rates.js
@@ -0,0 +1,107 @@
+const QueryTypes = require('sequelize').QueryTypes;
+const axios = require('axios');
+
+const TABLE_NAME = 'UserExchangeRates'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+
+    try {
+      await queryInterface.createTable(TABLE_NAME, {
+        id: {
+          type: Sequelize.INTEGER,
+          unique: true,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        baseCode: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        quoteCode: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        rate: {
+          type: Sequelize.FLOAT,
+          allowNull: true,
+          defaultValue: 1,
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.fn('NOW'),
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.fn('NOW'),
+        },
+      }, { transaction });
+
+      await queryInterface.addColumn(
+        TABLE_NAME,
+        'baseId',
+        {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'Currencies',
+            key: 'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        { transaction },
+      );
+
+      await queryInterface.addColumn(
+        TABLE_NAME,
+        'quoteId',
+        {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'Currencies',
+            key: 'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        { transaction },
+      );
+
+      await queryInterface.addColumn(
+        TABLE_NAME,
+        'userId',
+        {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        { transaction },
+      );
+
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+  down: async (queryInterface) => {
+    const transaction = await queryInterface.sequelize.transaction();
+
+    try {
+      await queryInterface.dropTable(TABLE_NAME, { transaction });
+
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+};
---
+++ src/models/UserExchangeRates.model.ts
@@ -0,0 +1,40 @@
+import {
+  Table,
+  Column,
+  Model,
+  ForeignKey,
+} from 'sequelize-typescript';
+import Currencies from './Currencies.model';
+import Users from './Users.model';
+
+@Table({ timestamps: true })
+export default class ExchangeRates extends Model {
+  @Column({
+    unique: true,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @ForeignKey(() => Users)
+  @Column({ allowNull: false })
+  userId: number;
+
+  @ForeignKey(() => Currencies)
+  @Column({ allowNull: false })
+  baseId: number;
+
+  @Column({ allowNull: false })
+  baseCode: string;
+
+  @ForeignKey(() => Currencies)
+  @Column({ allowNull: false })
+  quoteId: number;
+
+  @Column({ allowNull: false })
+  quoteCode: string;
+
+  @Column({ allowNull: true, defaultValue: 1 })
+  rate: number;
+}
