aff127670afb40fff20f563f94cb004470b11039
Dmitry Sviridenko <letehaha@gmail.com>
2023-06-30 18:12:27 +0300

    feat: Add simple auth/validate-token route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
+import { authenticateJwt } from '@middlewares/passport';
 import {
   login,
   register,
-} from '../controllers/auth.controller';
-import validation from '../middlewares/validations';
+  validateToken,
+} from '@controllers/auth.controller';
+import validation from '@middlewares/validations';
 
 const router = Router({});
 
+router.get('/validate-token', authenticateJwt, validateToken)
 router.post('/login', [], validation, login);
 router.post('/register', [], validation, register);
 
