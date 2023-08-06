c3fc28415035ba47ba89b66074a010d67c0ae38e
Evelina Kutalo <kutaloea@gmail.com>
2023-08-06 17:40:07 +0300

    feat: new auth tests

---
+++ src/controllers/auth.controller.e2e.ts
@@ -0,0 +1,108 @@
+import { API_ERROR_CODES, API_RESPONSE_STATUS } from 'shared-types';
+import { makeRequest } from '@tests/helpers';
+
+describe('Auth', () => {
+  describe('Login',  () => { 
+    it('should return correct error for unexisting user', async () => {
+      const res = await makeRequest({
+        method: 'post',
+        url: '/auth/login',
+        payload: {
+          username: 'unexisting-user',
+          password: 'unexisting-password',
+        },
+      });
+  
+      expect(res.statusCode).toEqual(404);
+      expect(res.body.status).toEqual(API_RESPONSE_STATUS.error);
+      expect(res.body.response.code).toEqual(API_ERROR_CODES.notFound);
+    })
+  
+    it('should return successful login response', async () => {
+      await makeRequest({
+        method: 'post',
+        url: '/auth/register',
+        payload: {
+          username: 'test_user',
+          password: 'test_user',
+        },
+      });
+      const res = await makeRequest({
+        method: 'post',
+        url: '/auth/login',
+        payload: {
+          username: 'test_user',
+          password: 'test_user',
+        },
+      });
+  
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.status).toEqual(API_RESPONSE_STATUS.success);
+      expect(res.body.response.token).toContain('Bearer ');
+    })
+  
+    it('should return error when using invalid password', async () => {
+      await makeRequest({
+        method: 'post',
+        url: '/auth/register',
+        payload: {
+          username: 'test_user',
+          password: 'test_user',
+        },
+      });
+      const res = await makeRequest({
+        method: 'post',
+        url: '/auth/login',
+        payload: {
+          username: 'test_user',
+          password: 'test_user1',
+        },
+      });
+  
+      expect(res.statusCode).toEqual(401);
+      expect(res.body.status).toEqual(API_RESPONSE_STATUS.error);
+      expect(res.body.response.code).toEqual(API_ERROR_CODES.invalidCredentials);
+    })
+  })
+
+  describe('Registration', () => {
+    it('should return successful registration', async () => {
+      const res = await makeRequest({
+        method: 'post',
+        url: '/auth/register',
+        payload: {
+          username: 'test_user',
+          password: 'test_user',
+        },
+      });
+  
+      expect(res.statusCode).toEqual(201);
+      expect(res.body.status).toEqual(API_RESPONSE_STATUS.success);
+      expect(res.body.response.user.username).toEqual('test_user');
+    })
+
+    it('should return error when register existing user', async () => {
+      await makeRequest({
+        method: 'post',
+        url: '/auth/register',
+        payload: {
+          username: 'test_user',
+          password: 'test_user',
+        },
+      });
+
+      const res = await makeRequest({
+        method: 'post',
+        url: '/auth/register',
+        payload: {
+          username: 'test_user',
+          password: 'test_user',
+        },
+      });
+  
+      expect(res.statusCode).toEqual(409);
+      expect(res.body.status).toEqual(API_RESPONSE_STATUS.error);
+      expect(res.body.response.code).toEqual(API_ERROR_CODES.userExists);
+    })
+  })  
+})
