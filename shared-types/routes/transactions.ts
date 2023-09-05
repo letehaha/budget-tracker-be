f918ce5726025cb859b071ca2ac9c43e26d9aa49
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-09-05 22:43:21 +0300

    chore: Fix types for GetTransactionsQuery

--- shared-types/routes/transactions.ts
+++ shared-types/routes/transactions.ts
@@ -2,16 +2,16 @@ import { TransactionModel, ACCOUNT_TYPES } from 'shared-types';
 import { BodyPayload, QueryPayload } from './index';
 
 export interface GetTransactionsQuery extends QueryPayload {
-  sort: 'ASC' | 'DESC';
-  includeUser: boolean;
-  includeAccount: boolean;
-  includeCategory: boolean;
-  includeAll: boolean;
-  nestedInclude: boolean;
-  limit: number;
-  from: number;
-  accountType: ACCOUNT_TYPES;
-  accountId: number;
+  sort?: 'ASC' | 'DESC';
+  includeUser?: boolean;
+  includeAccount?: boolean;
+  includeCategory?: boolean;
+  includeAll?: boolean;
+  nestedInclude?: boolean;
+  limit?: number;
+  from?: number;
+  accountType?: ACCOUNT_TYPES;
+  accountId?: number;
 }
 
 export type GetTransactionsResponse = TransactionModel[];
