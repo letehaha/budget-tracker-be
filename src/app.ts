a3ab0a42134ea155a22a15e780d51dea3d74153a
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2024-01-26 20:55:02 +0200

    feat: Enable CORS for localhost:8100

--- src/app.ts
+++ src/app.ts
@@ -59,6 +59,7 @@ app.use(
   cors({
     origin(requestOrigin, callback) {
       const ALLOWED_HOSTS = [
+        '127.0.0.1:8100',
         'budget-tracker.com:8100',
         '206.81.20.28:8081',
         'gamanets.money',
