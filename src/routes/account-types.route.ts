6f969b90db3da230edce157201e502a51412c545
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2024-01-28 13:26:06 +0200

    chore: Delete useless code related to account-types

--- src/routes/account-types.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAccountTypes } from '../controllers/account-types.controller';
-import validation from '../middlewares/validations';
-
-const router = Router({});
-
-router.get('/', [], validation, getAccountTypes);
-
-export default router;
