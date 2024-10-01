7bc914b61cbf48cb27bde846f43de8b853c228de
Evelina Kutalo <kutaloea@gmail.com>
2024-10-02 00:31:36 +0200

    tests: multicurrency-stats

--- src/services/stats/get-expenses-amount-for-period.e2e.ts
+++ src/services/stats/get-expenses-amount-for-period.e2e.ts
@@ -247,4 +247,112 @@ describe('Get expense amount for period', () => {
       expect(stats).toBe(expected);
     }
   });
+
+  it(`should display expences amount in different currencies with different accounts`, async () => {
+    //create 3 accounts with usd/uah/eur as the base currencies
+    const account = await helpers.createAccount({ raw: true });
+
+    const allCurrencies = await helpers.getAllCurrencies();
+    const uah = allCurrencies.find((i) => i.code === 'UAH')!;
+    const accountB = await helpers.createAccount({
+      payload: {
+        ...helpers.buildAccountPayload(),
+        currencyId: uah.id,
+      },
+      raw: true,
+    });
+
+    const eur = allCurrencies.find((i) => i.code === 'EUR')!;
+    const accountC = await helpers.createAccount({
+      payload: {
+        ...helpers.buildAccountPayload(),
+        currencyId: eur.id,
+      },
+      raw: true,
+    });
+
+    await helpers.makeRequest({
+      method: 'post',
+      url: '/user/currencies',
+      payload: {
+        currencies: [{ currencyId: uah.id }, { currencyId: eur.id }],
+      },
+      raw: false,
+    });
+
+    const pairs = [
+      { baseCode: 'USD', quoteCode: 'EUR', rate: 0.9 },
+      { baseCode: 'EUR', quoteCode: 'USD', rate: 1.11 },
+      { baseCode: 'USD', quoteCode: 'UAH', rate: 40 },
+      { baseCode: 'UAH', quoteCode: 'USD', rate: 0.025 },
+    ];
+
+    await helpers.editCurrencyExchangeRate({
+      pairs,
+      raw: false,
+    });
+
+    //create transactions with different currencies between 3 accounts
+    await Promise.all(
+      [
+        { amount: 1000, currency: 'USD', accountId: account.id },
+        { amount: 100, currency: 'UAH', accountId: accountB.id },
+        { amount: 50, currency: 'EUR', accountId: accountC.id },
+        { amount: 1000, currency: 'USD', accountId: account.id },
+        { amount: 350, currency: 'UAH', accountId: accountB.id },
+        { amount: 1500, currency: 'EUR', accountId: accountC.id },
+        { amount: 100, currency: 'USD', accountId: account.id },
+        { amount: 700, currency: 'UAH', accountId: accountB.id },
+        { amount: 500, currency: 'EUR', accountId: accountC.id },
+        { amount: 450, currency: 'UAH', accountId: accountB.id },
+      ].map((payload) =>
+        helpers.createTransaction({
+          payload: {
+            ...helpers.buildTransactionPayload({
+              ...payload,
+            }),
+          },
+          raw: true,
+        }),
+      ),
+    );
+    // calculate the total balance in USD
+    const transactions = [
+      { amount: 1000, currency: 'USD' },
+      { amount: 100, currency: 'UAH' },
+      { amount: 50, currency: 'EUR' },
+      { amount: 1000, currency: 'USD' },
+      { amount: 350, currency: 'UAH' },
+      { amount: 1500, currency: 'EUR' },
+      { amount: 100, currency: 'USD' },
+      { amount: 700, currency: 'UAH' },
+      { amount: 500, currency: 'EUR' },
+      { amount: 450, currency: 'UAH' },
+    ];
+
+    const exchangeRates = {
+      UAH: 0.025, // 1 UAH = 0.025 USD
+      EUR: 1.11, // 1 EUR = 1.11 USD
+    };
+
+    // calculate total expenses in USD
+    const totalInUSD = transactions.reduce((total, { amount, currency }) => {
+      if (currency === 'USD') {
+        return total + amount;
+      } else if (currency === 'UAH') {
+        return Math.floor(total + amount * exchangeRates.UAH);
+      } else if (currency === 'EUR') {
+        return Math.floor(total + amount * exchangeRates.EUR);
+      }
+      return total;
+    }, 0);
+
+    const stats = await helpers.makeRequest({
+      method: 'get',
+      url: '/stats/expenses-amount-for-period',
+      raw: true,
+    });
+
+    expect(stats).toBe(totalInUSD);
+  });
 });
