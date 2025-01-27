0a1048ae8cf46221f6dc008a6f5a345a65c58136
Dany <lysakdanil@gmail.com>
2025-01-27 11:12:35 +0200

    Feat: checking categories before settings update

--- src/services/user-settings/update-settings.ts
+++ src/services/user-settings/update-settings.ts
@@ -1,8 +1,28 @@
 import UserSettings, { type SettingsSchema } from '@models/UserSettings.model';
+import Categories from '@models/Categories.model';
 import { withTransaction } from '../common';
+import { ValidationError } from '@js/errors';
 
 export const updateUserSettings = withTransaction(
-  async ({ userId, settings }: { userId: number; settings: SettingsSchema }): Promise<SettingsSchema> => {
+  async ({ userId, settings }: { userId: number; settings: SettingsSchema; }): Promise<SettingsSchema> => {
+    const excludedCategories = settings.stats?.expenses?.excludedCategories;
+    
+    if (excludedCategories && excludedCategories.length > 0) {
+      const existingCount = await Categories.count({
+        where: { id: excludedCategories }
+      });
+
+      if (existingCount !== excludedCategories.length) {
+        throw new ValidationError({
+          message: 'One or more excluded categories not found',
+          details: {
+            invalidCategories: excludedCategories.filter(async id => 
+              !(await Categories.findByPk(id))
+            )
+          }
+        });
+      }
+    }
     const [existingSettings, created] = await UserSettings.findOrCreate({
       where: { userId },
       defaults: { settings },
