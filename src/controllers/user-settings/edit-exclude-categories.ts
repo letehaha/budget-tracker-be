361554649922c8e865fedfaca942c673b734274a
Danylo Lysak <lysakdanil@gmail.com>
2025-02-16 17:00:03 +0200

    Feat: Add 'add/remove' exclude methods to categories (#153)
    * Feat: Add 'add/remove' exclude methods to categories
    
    * Fix: delete useless files and code
    
    * Feat: change code
    
    * fix: Re-wright code
    
    * Fix: Fix all comments
    
    * Fix: fixing comments/v2
    
    * Feat: Adding test with mocks data
    
    * Feat: Add tests to edit-excluded-categories
    
    * Fix: edit-exclude-category.e2e test
    
    * Fix: Fix and adding new test to edit-excluded-categories
    
    * Fix: Use "SettingsSchema" instead of doing new z.infer
    
    ---------
    
    Co-authored-by: Dmytro <dmytro.svyrydenko@gmail.com>

---
+++ src/controllers/user-settings/edit-exclude-categories.ts
@@ -0,0 +1,36 @@
+import { API_RESPONSE_STATUS } from 'shared-types';
+import { z } from 'zod';
+import { recordId } from '@common/lib/zod/custom-types';
+import { editExcludedCategories } from '@services/user-settings/edit-excluded-categories';
+import { errorHandler } from '@controllers/helpers';
+import { CustomResponse } from '@common/types';
+
+export const editExcludedCategoriesHandler = async (
+  req,
+  res: CustomResponse
+) => {
+  try {
+    const { addIds, removeIds } = req.validated.body;
+    const { user } = req;
+
+    const updatedCategories = await editExcludedCategories({
+      userId: user.id,
+      addIds,
+      removeIds,
+    });
+    
+    res.status(200).json({
+      status: API_RESPONSE_STATUS.success,
+      response: updatedCategories,
+    });
+  } catch (error) {
+    errorHandler(res, error);
+  }
+};
+
+export const editExcludedCategoriesSchema = z.object({
+  body: z.object({
+    addIds: z.array(recordId()).optional().default([]),
+    removeIds: z.array(recordId()).optional().default([]),
+  }),
+});
