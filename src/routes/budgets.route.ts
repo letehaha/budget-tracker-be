09de6bc1fc8c0bb190150f962e25ace80b3744fa
Dany <lysakdanil@gmail.com>
2025-03-28 15:25:24 +0200

    Feat: Adding 'add-transaction-to-budget' functionality

--- src/routes/budgets.route.ts
+++ src/routes/budgets.route.ts
@@ -5,12 +5,14 @@ import { getBudgets, getBudgetById } from '@controllers/budgets/get-budgets';
 import { createBudget, createBudgetSchema } from '@controllers/budgets/create-budget';
 import { deleteBudget, deleteBudgetSchema } from '@controllers/budgets/delete-budgets';
 import { editBudget, editBudgetSchema } from '@controllers/budgets/edit-budgets';
+import { addTransactionsToBudget, addTransactionsToBudgetSchema } from '@controllers/budgets/add-transaction-to-budget';
 
 const router = Router({});
 
 router.get('/', authenticateJwt, getBudgets);
 router.get('/:id', authenticateJwt, getBudgetById)
 router.post('/', authenticateJwt, validateEndpoint(createBudgetSchema), createBudget);
+router.post('/:id/transactions', authenticateJwt, validateEndpoint(addTransactionsToBudgetSchema), addTransactionsToBudget)
 router.put('/:id', authenticateJwt, validateEndpoint(editBudgetSchema), editBudget);
 router.delete('/:id', authenticateJwt, validateEndpoint(deleteBudgetSchema), deleteBudget);
 
